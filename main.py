1d86f962632630588fb9f59703c500fa3e1430b6
jeremiah.1582@googlemail.com <jeremiah.1582@googlemail.com>
2023-10-20 11:40:27 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+
+# Given data
+temperatures_in_celsius = [20.5, 22.4, 19.8, 21.3, 18.9]
+air_quality_index = [45, 62, 150, 55, 112]  # Values > 100 are considered "poor" air quality.
+pollutants = ["CO2", "Ozone", "CO2", "SO2", "CO2"]
+humidity_levels = [45, 55, 60, 48, 53]
